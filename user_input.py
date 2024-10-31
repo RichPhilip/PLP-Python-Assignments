0fd966023257c165c048a77a8c01b05e864887b9
RichPhilip <onuohaphilip444@gmail.com>
2024-10-31 17:42:28 +0100

    done

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# Ask for user inputs
+
+name = input("What is your name? :")
+age = input("How old are you? :")
+location = input("Where do you live? :")
+
+
+# Print out a personalized message 
+print(f"Hello {name}, you are {age} and lives im {location}")
